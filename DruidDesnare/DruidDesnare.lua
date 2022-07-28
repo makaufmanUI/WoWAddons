175ef13bbbaddb508ca3cef8f9fa193aa2b0c999
Matt Kaufman <98175105+makaufmanUI@users.noreply.github.com>
2022-07-28 16:31:03 -0500

    Add files via upload

---
+++ DruidDesnare/DruidDesnare.lua
@@ -0,0 +1,290 @@
+
+SLASH_CLR1 = "/clr"
+
+
+AllSpellIDs = {
+    ----------------
+	-- Druid
+	----------------
+	[339]    = "Root",				-- Entangling Roots (rank 1)
+	[1062]   = "Root",				-- Entangling Roots (rank 2)
+	[5195]   = "Root",				-- Entangling Roots (rank 3)
+	[5196]   = "Root",				-- Entangling Roots (rank 4)
+	[9852]   = "Root",				-- Entangling Roots (rank 5)
+	[9853]   = "Root",				-- Entangling Roots (rank 6)
+	[26989]  = "Root",				-- Entangling Roots (rank 7)
+    [19975]  = "Root",				-- Entangling Roots (rank 1) (Nature's Grasp talent)
+	[19974]  = "Root",				-- Entangling Roots (rank 2) (Nature's Grasp talent)
+	[19973]  = "Root",				-- Entangling Roots (rank 3) (Nature's Grasp talent)
+	[19972]  = "Root",				-- Entangling Roots (rank 4) (Nature's Grasp talent)
+	[19971]  = "Root",				-- Entangling Roots (rank 5) (Nature's Grasp talent)
+	[19970]  = "Root",				-- Entangling Roots (rank 6) (Nature's Grasp talent)
+	[27010]  = "Root",				-- Entangling Roots (rank 6) (Nature's Grasp talent)
+    ----------------
+	-- Hunter
+	----------------
+    [19306]  = "Root",				-- Counterattack (talent) (rank 1)
+	[20909]  = "Root",				-- Counterattack (talent) (rank 2)
+	[20910]  = "Root",				-- Counterattack (talent) (rank 3)
+	[27067]  = "Root",				-- Counterattack (talent) (rank 4)
+	[19229]  = "Root",				-- Improved Wing Clip (talent)
+	[19185]  = "Root",				-- Entrapment (talent)
+        ----------------
+		-- Hunter Pets
+		----------------
+        [4167]   = "Root",				-- Web
+	    [4168]   = "Root",				-- Web II
+	    [4169]   = "Root",				-- Web III
+        [25999]  = "Root",				-- Boar Charge (Boar)
+    ----------------
+	-- Mage
+	----------------
+    [122]    = "Root",				-- Frost Nova (rank 1)
+	[865]    = "Root",				-- Frost Nova (rank 2)
+	[6131]   = "Root",				-- Frost Nova (rank 3)
+	[10230]  = "Root",				-- Frost Nova (rank 4)
+	[27088]  = "Root",				-- Frost Nova (rank 5)
+	[12494]  = "Root",				-- Frostbite (talent)
+    [12484]  = "Snare",				-- Chilled (rank 1) (Improved Blizzard talent)
+	[12485]  = "Snare",				-- Chilled (rank 2) (Improved Blizzard talent)
+	[12486]  = "Snare",				-- Chilled (rank 3) (Improved Blizzard talent)
+	[120]    = "Snare",				-- Cone of Cold (rank 1)
+	[8492]   = "Snare",				-- Cone of Cold (rank 2)
+	[10159]  = "Snare",				-- Cone of Cold (rank 3)
+	[10160]  = "Snare",				-- Cone of Cold (rank 4)
+	[10161]  = "Snare",				-- Cone of Cold (rank 5)
+	[27087]  = "Snare",				-- Cone of Cold (rank 6)
+	[116]    = "Snare",				-- Frostbolt (rank 1)
+	[205]    = "Snare",				-- Frostbolt (rank 2)
+	[837]    = "Snare",				-- Frostbolt (rank 3)
+	[7322]   = "Snare",				-- Frostbolt (rank 4)
+	[8406]   = "Snare",				-- Frostbolt (rank 5)
+	[8407]   = "Snare",				-- Frostbolt (rank 6)
+	[8408]   = "Snare",				-- Frostbolt (rank 7)
+	[10179]  = "Snare",				-- Frostbolt (rank 8)
+	[10180]  = "Snare",				-- Frostbolt (rank 9)
+	[10181]  = "Snare",				-- Frostbolt (rank 10)
+	[25304]  = "Snare",				-- Frostbolt (rank 11)
+	[27071]  = "Snare",				-- Frostbolt (rank 12)
+	[27072]  = "Snare",				-- Frostbolt (rank 13)
+	[38697]  = "Snare",				-- Frostbolt (rank 14)
+	[31589]  = "Snare",				-- Slow (talent)
+        ----------------
+		-- Mage Water Elemental
+		----------------
+		[33395]  = "Root",				-- Freeze
+    ----------------
+	-- Paladin
+	----------------
+    [31935]  = "Snare",				-- Avenger's Shield (rank 1) (talent)
+	[32699]  = "Snare",				-- Avenger's Shield (rank 2) (talent)
+	[32700]  = "Snare",				-- Avenger's Shield (rank 3) (talent)
+    ----------------
+	-- Priest
+	----------------
+    [44041]  = "Root",				-- Chastise (rank 1)
+	[44043]  = "Root",				-- Chastise (rank 2)
+	[44044]  = "Root",				-- Chastise (rank 3)
+	[44045]  = "Root",				-- Chastise (rank 4)
+	[44046]  = "Root",				-- Chastise (rank 5)
+	[44047]  = "Root",				-- Chastise (rank 6)
+    [15407]  = "Snare",				-- Mind Flay (talent) (rank 1)
+	[17311]  = "Snare",				-- Mind Flay (talent) (rank 2)
+	[17312]  = "Snare",				-- Mind Flay (talent) (rank 3)
+	[17313]  = "Snare",				-- Mind Flay (talent) (rank 4)
+	[17314]  = "Snare",				-- Mind Flay (talent) (rank 5)
+	[18807]  = "Snare",				-- Mind Flay (talent) (rank 6)
+	[25387]  = "Snare",				-- Mind Flay (talent) (rank 7)
+    ----------------
+	-- Rogue
+	----------------
+    [3409]   = "Snare",				-- Crippling Poison (rank 1)
+	[11201]  = "Snare",				-- Crippling Poison (rank 2)
+	[26679]  = "Snare",				-- Deadly Throw (rank 1)
+	[31125]  = "Snare",				-- Dazed (Blade Twisting) (talent)
+    ----------------
+	-- Shaman
+	----------------
+    [8056]   = "Snare",				-- Frost Shock (rank 1)
+	[8058]   = "Snare",				-- Frost Shock (rank 2)
+	[10472]  = "Snare",				-- Frost Shock (rank 3)
+	[10473]  = "Snare",				-- Frost Shock (rank 4)
+	[25464]  = "Snare",				-- Frost Shock (rank 5)
+	[3600]   = "Snare",				-- Earthbind (Earthbind Totem)
+    ----------------
+	-- Warrior
+	----------------
+    [23694]  = "Root",				-- Improved Hamstring (talent)
+    [1715]   = "Snare",				-- Hamstring (rank 1)
+	[7372]   = "Snare",				-- Hamstring (rank 2)
+	[7373]   = "Snare",				-- Hamstring (rank 3)
+	[25212]  = "Snare",				-- Hamstring (rank 4)
+    ----------------
+	-- Other
+	----------------
+    [8312]   = "Root",				-- Trap (Hunting Net trinket)
+    [8346]   = "Root",				-- Mobility Malfunction (trinket)
+	[13099]  = "Root",				-- Net-o-Matic (trinket)
+	[13119]  = "Root",				-- Net-o-Matic (trinket)
+	[13138]  = "Root",				-- Net-o-Matic (trinket)
+	[16566]  = "Root",				-- Net-o-Matic (trinket)
+    [9915]   = "Root",				-- Frost Nova
+	[14907]  = "Root",				-- Frost Nova
+    [22645]  = "Root",				-- Frost Nova
+    [23115]  = "Snare",				-- Frost Shock
+	[19133]  = "Snare",				-- Frost Shock
+	[21030]  = "Snare",				-- Frost Shock
+	[11538]  = "Snare",				-- Frostbolt
+	[21369]  = "Snare",				-- Frostbolt
+	[20297]  = "Snare",				-- Frostbolt
+	[20806]  = "Snare",				-- Frostbolt
+	[20819]  = "Snare",				-- Frostbolt
+	[12737]  = "Snare",				-- Frostbolt
+	[20792]  = "Snare",				-- Frostbolt
+	[20822]  = "Snare",				-- Frostbolt
+	[23412]  = "Snare",				-- Frostbolt
+	[24942]  = "Snare",				-- Frostbolt
+	[23102]  = "Snare",				-- Frostbolt
+	[20828]  = "Snare",				-- Cone of Cold
+	[22746]  = "Snare",				-- Cone of Cold
+    [16094]  = "Snare",				-- Frost Breath
+	[16340]  = "Snare",				-- Frost Breath
+    [20654]  = "Root",				-- Entangling Roots
+	[22800]  = "Root",				-- Entangling Roots
+	[20699]  = "Root",				-- Entangling Roots
+    [12520]  = "Root",				-- Teleport from Azshara Tower
+	[12521]  = "Root",				-- Teleport from Azshara Tower
+	[12509]  = "Root",				-- Teleport from Azshara Tower
+	[12023]  = "Root",				-- Web
+	[13608]  = "Root",				-- Hooked Net
+	[10017]  = "Root",				-- Frost Hold
+	[23279]  = "Root",				-- Crippling Clip
+	[3542]   = "Root",				-- Naraxis Web
+	[5567]   = "Root",				-- Miring Mud
+	[5424]   = "Root",				-- Claw Grasp
+	[4246]   = "Root",				-- Clenched Pinchers
+	[5219]   = "Root",				-- Draw of Thistlenettle
+	[9576]   = "Root",				-- Lock Down
+	[7950]   = "Root",				-- Pause
+	[7761]   = "Root",				-- Shared Bonds
+	[6714]   = "Root",				-- Test of Faith
+	[6716]   = "Root",				-- Test of Faith
+    [27868]  = "Root",				-- Freeze (Magister's and Sorcerer's Regalia sets)
+	[17333]  = "Root",				-- Spider's Kiss (Spider's Kiss set)
+    [47407]  = "Root",				-- Direbrew's Disarm (precast)
+	[47411]  = "Root",				-- Direbrew's Disarm (spin)
+	[43207]  = "Root",				-- Headless Horseman Climax - Head's Breath
+	[43049]  = "Root",				-- Upset Tummy
+	[31287]  = "Root",				-- Entangling Roots
+	[31290]  = "Root",				-- Net
+	[31409]  = "Root",				-- Wild Roots
+	[33356]  = "Root",				-- Self Root Forever
+	[33844]  = "Root",				-- Entangling Roots
+	[34080]  = "Root",				-- Riposte Stance
+	[34569]  = "Root",				-- Chilled Earth
+	[34779]  = "Root",				-- Freezing Circle
+	[35234]  = "Root",				-- Strangling Roots
+	[35247]  = "Root",				-- Choking Wound
+	[35327]  = "Root",				-- Jackhammer
+	[39194]  = "Root",				-- Jackhammer
+	[36252]  = "Root",				-- Felforge Flames
+	[36734]  = "Root",				-- Test Whelp Net
+	[37823]  = "Root",				-- Entangling Roots
+	[38033]  = "Root",				-- Frost Nova
+	[38035]  = "Root",				-- Freeze
+	[38051]  = "Root",				-- Fel Shackles
+	[38338]  = "Root",				-- Net
+	[38505]  = "Root",				-- Shackle
+	[39268]  = "Root",				-- Chains of Ice
+	[40363]  = "Root",				-- Entangling Roots
+	[40525]  = "Root",				-- Rizzle's Frost Grenade
+	[40590]  = "Root",				-- Rizzle's Frost Grenade (Self
+	[40727]  = "Root",				-- Icy Leap
+	[40875]  = "Root",				-- Freeze
+	[41981]  = "Root",				-- Dust Field
+	[42716]  = "Root",				-- Self Root Forever (No Visual)
+	[43130]  = "Root",				-- Creeping Vines
+	[43150]  = "Root",				-- Claw Rage
+	[43426]  = "Root",				-- Frost Nova
+	[43585]  = "Root",				-- Entangle
+	[45255]  = "Root",				-- Rocket Chicken
+	[45905]  = "Root",				-- Frost Nova
+    [29957]  = "Snare",				-- Frostbolt Volley
+    [30942]  = "Snare",				-- Frostbolt
+	[30981]  = "Snare",				-- Crippling Poison
+	[31296]  = "Snare",				-- Frostbolt
+    [32984]  = "Snare",				-- Frostbolt
+	[33047]  = "Snare",				-- Void Bolt
+	[34214]  = "Snare",				-- Frost Touch
+	[34347]  = "Snare",				-- Frostbolt
+    [35263]  = "Snare",				-- Frost Attack
+	[35316]  = "Snare",				-- Frostbolt
+    [36148]  = "Snare",				-- Chill Nova
+	[36278]  = "Snare",				-- Blast Wave
+	[36279]  = "Snare",				-- Frostbolt
+    [38534]  = "Snare",				-- Frostbolt
+	[38536]  = "Snare",				-- Blast Wave
+	[38663]  = "Snare",				-- Slow
+	[38712]  = "Snare",				-- Blast Wave
+    [38952]  = "Snare",				-- Frost Arrow
+    [40429]  = "Snare",				-- Frostbolt
+	[40430]  = "Snare",				-- Frostbolt
+    [41486]  = "Snare",				-- Frostbolt
+	[42396]  = "Snare",				-- Mind Flay
+	[42803]  = "Snare",				-- Frostbolt
+	[43428]  = "Snare",				-- Frostbolt
+    [44289]  = "Snare",				-- Crippling Poison
+    [46984]  = "Snare",				-- Cone of Cold
+	[46987]  = "Snare",				-- Frostbolt
+	[6907]   = "Debuff",			-- Diseased Slime
+}
+
+local function ClearChatWindow()
+    print(" "); print(" ");
+    print(" "); print(" ");
+    print(" "); print(" ");
+    print(" "); print(" ");
+    print(" "); print(" ");
+    print(" "); print(" ");
+end
+
+
+local function HandleDebuff()
+    for i=1,40 do
+        local name, _, _, _, _, _, _, _, _, spellId = UnitDebuff("player", i)
+        if name then
+            if spellId and AllSpellIDs[spellId] then
+                if AllSpellIDs[spellId] == "Root" then
+                    print(" ")
+                    print("  You  are  rooted  !  (" .. name .. ")")
+                    print(" ")
+                elseif AllSpellIDs[spellId] == "Snare" then
+                    print(" ")
+                    print("  You  are  snared  !  (" .. name .. ")")
+                    print(" ")
+				elseif AllSpellIDs[spellId] == "Debuff" then
+					print(" ")
+					print("  You  are  rooted  !  (" .. name .. ")")
+					print(" ")
+                end
+            elseif spellId then
+				if name ~= "Dazed" then
+                	print(" ")
+					print('--------------')
+                	print("Debuffed with:  " .. name .. "  ( ID: " .. spellId .. " )")
+                	print("If it can be shapeshifted out of, it needs to be added to the AllSpellIDs table.")
+					print('--------------')
+                	print(" ")
+				end
+            end
+        else
+            break
+        end
+    end
+end
+local RootWatcher = CreateFrame("Frame")
+RootWatcher:RegisterUnitEvent("UNIT_AURA", "player")
+RootWatcher:SetScript("OnEvent", HandleDebuff)
+
+
+SlashCmdList["CLR"] = ClearChatWindow
---
+++ DruidDesnare/snare_watcher.py
@@ -0,0 +1,38 @@
+from stuff import delay
+from stuff import Screen
+from stuff import Keyboard
+from stuff import any_match
+MAIN = __name__ == "__main__"
+
+delay(5)
+screen = Screen()
+keyboard = Keyboard()
+TRIGGERS = ("rooted", "snared")
+
+def Clear():
+    # 0.4ms to execute
+    keyboard.type('/clr')
+    keyboard.press('enter')
+
+def RecastShapeshift():
+    # 1.4ms to execute
+    keyboard.press('f')
+    keyboard.press('f')
+
+def Loop():
+    # 130ms to execute
+    global line
+    global match
+    match = False
+    chat = screen.read_chat()
+    for line in chat:
+        if any_match(TRIGGERS, line):
+            match = True
+            RecastShapeshift(); return
+
+if MAIN:
+    while True:
+        Loop()
+        if match: Clear()
+
+
